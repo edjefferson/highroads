2a8d541d6a0948ca6f0ea0cdbf2b38e4b8109712
Ed Jefferson <e.jefferson@gmail.com>
2018-09-17 15:15:40 +0100

    a-l

--- convert_to_grids.py
+++ convert_to_grids.py
@@ -34,12 +34,12 @@ def assign_to_grid(file_name):
             queried.sort_values(by=['latitude','longitude']).to_csv("grids/n%sw%s%s.csv" % (lat,zero_pad,-lng), mode='a', header=False)
 
 
-            print(queried)
+            print("grid n%sw%s%s" % (lat,zero_pad,-lng))
 
 files = sorted(os.listdir('output/all_roads_csvs/'))
 
 for file in files:
-    if file[-4:] == ".csv" and file[:2] > "LA":
+    if file[-4:] == ".csv" and file[:2] <= "LA":
         print(file)
         assign_to_grid('output/all_roads_csvs/' + file)
         #time.sleep(5)
