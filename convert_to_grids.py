cca4026bae55158c752049d34f3eb4184cbed02f
Ed Jefferson <e.jefferson@gmail.com>
2018-09-17 15:07:18 +0100

    grid code

--- convert_to_grids.py
+++ convert_to_grids.py
@@ -1,6 +1,8 @@
 import csv
 import pandas
 import math
+import time
+import os
 
 def assign_to_grid(file_name):
     df = pandas.read_csv(file_name,
@@ -29,9 +31,18 @@ def assign_to_grid(file_name):
             zero_pad = ""
         queried = df.query('latitude >= @lat & latitude < @lat + 1 & longitude >= @lng & longitude < @lng + 1')
         if queried.size > 0:
-            queried.sort_values(by=['latitude','longitude']).to_csv("n%sw%s%s.csv" % (lat,zero_pad,-lng), mode='a', header=False)
+            queried.sort_values(by=['latitude','longitude']).to_csv("grids/n%sw%s%s.csv" % (lat,zero_pad,-lng), mode='a', header=False)
 
 
             print(queried)
 
-assign_to_grid("nytest.csv")
+files = sorted(os.listdir('output/all_roads_csvs/'))
+
+for file in files:
+    if file[-4:] == ".csv" and file[:2] > "LA":
+        print(file)
+        assign_to_grid('output/all_roads_csvs/' + file)
+        #time.sleep(5)
+
+
+#("nytest.csv")
