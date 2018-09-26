73a1f3d861db5c0a7f1e26edff68255f233f5fd1
Ed Jefferson <e.jefferson@gmail.com>
2018-09-26 16:53:24 +0100

    grid matcher working???

---
+++ gridchecker.py
@@ -0,0 +1,19 @@
+import csv
+import pandas
+import math
+import time
+import os
+
+files = sorted(os.listdir('testing'))
+
+df = pandas.read_csv("coded_master_list_ned_img.csv",
+                        header=0)
+
+print(df)
+for file in files:
+    if file[-4:] == ".csv":
+        print(file)
+        start, end = file[:-4].split("w")
+        new_start = int(start[1:]) + 1
+        code = "n%sw%s" % (new_start,end)
+        print(df[df.code == code].shape[0])
